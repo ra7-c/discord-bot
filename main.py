c1eec9f5f0efa286a77035a9e812ac966fbf8a37
ra7-c <utig6362@gmail.com>
2026-08-09 01:34:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
-    channel_id = 1341457814349144124  # آي دي الروم الصوتي حقك
+    channel_id = 1341457814349144124  # أي دي الروم الصوتي حقك
     channel = bot.get_channel(channel_id)
     if channel:
         try:
@@ -22,5 +22,4 @@ async def on_ready():
     else:
         print("Voice channel not found!")
 
-# استبدل هذا التوكن بالتوكن الخاص ببوتك
-bot.run("MTUzNTc1NjU3MTA1NTc1NTM5NA.Gutf8G.7gfVthbvUSINd5E1_0xxoQZR-kqc1K-RgSAstQ")
+bot.run("MTUzNTc1NjU3MTA1NTc1NTM5NC5GdTJHOy5jN2FjVGh2VUsINDByLUtqc0FjL1ItR3dTYXQt")
